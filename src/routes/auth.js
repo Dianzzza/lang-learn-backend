ea163bbfe35e9bd1b242bf2adab914eb01d2dc3f
Dawid Bodanka <dawidbodanka@gmail.com>
2025-11-04 20:19:10 +0100

    zamiana struktury

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const prisma = require("../prismaClient");
-const sendEmail = require("../utils/sendEmail");
+const sendEmail = require("../../utils/sendEmail");
 
 const router = express.Router();
 
